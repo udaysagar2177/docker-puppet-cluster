664dcbceee675cf30627327c8b90ae3b3eb87796
udaysagar2177 <udaysagar.2177@gmail.com>
2015-11-03 23:16:48 +0000

    Check for running puppet container before stopping it Get source package for puppet on Ubuntu 12.04

--- destroy_puppet.sh
+++ destroy_puppet.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-set -xe
 
-docker stop puppet
+if [[ $(docker ps -a | grep "puppet") ]]; then
+   docker rm -f  puppet
+else
+   echo "container not found for puppet"
+fi
 os_array=(pa_ubuntu1204 pa_ubuntu1404 pa_ubuntu1504 pa_centos5 pa_centos6 pa_centos7 pa_amzlinux201409 pa_amzlinux201503 pa_amzlinux201509)
 for os in ${os_array[@]}
 do
