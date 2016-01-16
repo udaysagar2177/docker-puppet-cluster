fa57123dadc15451334c01e1acd5d46bd21d9781
udaysagar2177 <udaysagar.2177@gmail.com>
2016-01-15 21:47:06 -0800

    Check if all docker images exist on the system Use CONTAINERS_LIST_FILE to maintain the names of launched containers Use the name puppet_agent_<serial_number> for the containers Decrease sleeping times

--- setup_puppet_cluster.sh
+++ setup_puppet_cluster.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xe
 
+BUILD="false"
+NO_CACHE="false"
+
 while [[  "$#" -gt "0" ]]
 do
   key="$1"
@@ -18,19 +21,53 @@ do
   shift
 done
 
-
 # Check NO_CACHE and build images with out using cache
 if [ "$NO_CACHE" = "true" ]; then
   if [ "$BUILD" = "true" ]; then
     no_cache="--no-cache"
   else
     echo "-n|--no-cache is valid only if -b|--build is given"
+    exit 2;
   fi
 else
    no_cache=""
 fi
 
-os_array=(pa_ubuntu1204 pa_ubuntu1404 pa_ubuntu1504 pa_centos5 pa_centos6 pa_centos7 pa_amzlinux201409 pa_amzlinux201503 pa_amzlinux201509)
+os_array=(pa_ubuntu1204 pa_ubuntu1404 pa_ubuntu1504 pa_centos5 pa_centos6 pa_amzlinux201409 pa_amzlinux201503 pa_amzlinux201509)
+
+# Build the given docker image
+build_docker_image(){
+  $os=$1
+  (
+    cd ./puppetagents
+    if [ -d $os ]; then
+      cd $os
+      cp ../install_software.sh .
+      cp ../run.sh .
+      cp ../puppet_slave.conf .
+      cp ../check_puppet_status.rb .
+      docker build $no_cache -t $os .
+      rm install_software.sh run.sh puppet_slave.conf check_puppet_status.rb
+    fi
+  )
+}
+
+check_for_all_docker_images() {
+  docker_image_list=("${os_array[@]}" "puppet")
+  for image_name in ${docker_image_list[@]}
+  do
+    if [[ -z $(docker images | grep -w "$image_name" | grep -Ev "grep") ]]; then
+      BUILD="true"
+      unset docker_image_list
+      return
+    fi
+  done
+  unset docker_image_list
+}
+
+if [[ $BUILD == "false" ]]; then
+  check_for_all_docker_images
+fi
 
 # build docker images only if build argument is passed
 if [ "$BUILD" = "true" ]; then
@@ -38,28 +75,19 @@ if [ "$BUILD" = "true" ]; then
     cd ./puppetmaster
     docker build $no_cache -t puppet .
   )
-  (
-    cd ./puppetagents
-    for os in  ${os_array[@]}
-    do
-        if [ -d $os ]; then
-          cd $os
-          cp ../install_software.sh .
-          cp ../run.sh .
-          cp ../puppet_slave.conf .
-          cp ../check_puppet_status.rb .
-          docker build $no_cache -t $os .
-          rm install_software.sh run.sh puppet_slave.conf check_puppet_status.rb
-          cd ..
-        fi
-    done
-  )
+  for os in  ${os_array[@]}
+  do
+    build_docker_image $os
+  done
 fi
 
 # Check and stop running puppet cluster
 ./shutdown_puppet_cluster.sh
 
-# Start Puppet agents
+# File to save launched container names
+CONTAINERS_LIST_FILE="cluster.txt"
+
+# Start Puppetmaster
 docker run \
     -d \
     -h "puppet" \
@@ -67,26 +95,27 @@ docker run \
     -v $(pwd)/puppetmaster/manifests:/etc/puppet/manifests \
     -v $(pwd)/puppetmaster/modules:/etc/puppet/modules \
     puppet
+echo "puppet" > $CONTAINERS_LIST_FILE
 echo "Sleeping to let the puppetmaster start and initialize"
-sleep 10
+sleep 3
 
+AGENT_SERIAL_NUMBER=0
 # Start Puppet agents
-( 
-  cd ./puppetagents
-  for os in  ${os_array[@]}
-  do
-      if [ -d $os ]; then
-        random_runinterval=$(awk -vmin=60 -vmax=110 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
-        docker run \
-            -d \
-            --name $os \
-            -e "random_runinterval=${random_runinterval}" \
-            -h $os \
-            --link puppet:puppet \
-            $os
-        echo "waiting for this puppet agent to shake hands with puppet master"
-        echo "random runinterval for this agent is ${random_runinterval}"
-        sleep 2
-      fi
-  done
-)
+for os in  ${os_array[@]}
+do
+    if [ -d ./puppetagents/$os ]; then
+      random_runinterval=$(awk -vmin=60 -vmax=110 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+      AGENT_SERIAL_NUMBER=$(( $AGENT_SERIAL_NUMBER + 1 ))
+      docker run \
+          -d \
+          --name puppet_agent_$AGENT_SERIAL_NUMBER \
+          -e "random_runinterval=${random_runinterval}" \
+          -h $os \
+          --link puppet:puppet \
+          $os
+      echo "puppet_agent_${AGENT_SERIAL_NUMBER}" >> $CONTAINERS_LIST_FILE
+      echo "waiting for puppet agent ${AGENT_SERIAL_NUMBER} to shake hands with puppet master"
+      echo "random runinterval for this agent is ${random_runinterval}"
+      sleep 1
+    fi
+done
